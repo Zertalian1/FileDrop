ed63a0e9d0e11a4341fcb1d7f3ea988d632afa3f
r.kotov <r.kotov@g.nsu.ru>
2022-12-06 13:24:51 +0700

    Инициализация проекта FileDrop

---
+++ FileDrop/src/main/java/ru/ccfit/filedrop/FileDropApplication.java
@@ -0,0 +1,13 @@
+package ru.ccfit.filedrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDropApplication.class, args);
+    }
+
+}
---
+++ FileDrop/src/test/java/ru/ccfit/filedrop/FileDropApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ccfit.filedrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
