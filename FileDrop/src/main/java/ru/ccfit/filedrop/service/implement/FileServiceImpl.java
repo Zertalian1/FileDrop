435a6956bd749f45a69514563859e81c61e5eea1
r.kotov <r.kotov@g.nsu.ru>
2022-12-12 13:51:21 +0700

    Создал сервисы для Order и File

---
+++ FileDrop/src/main/java/ru/ccfit/filedrop/service/implement/FileServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.ccfit.filedrop.service.implement;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.ccfit.filedrop.service.interfaces.FileService;
+
+@Service
+@AllArgsConstructor
+public class FileServiceImpl implements FileService {
+}
---
+++ FileDrop/src/main/java/ru/ccfit/filedrop/service/implement/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.ccfit.filedrop.service.implement;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.ccfit.filedrop.service.interfaces.OrderService;
+
+@Service
+@AllArgsConstructor
+public class OrderServiceImpl implements OrderService {
+}
